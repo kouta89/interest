45dd11479edd115e5400fefa5b0eb8921346d004
geromon <123849679+geromon89@users.noreply.github.com>
2023-05-05 02:02:39 +0000

    [add]update

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,9 @@
 class HomesController < ApplicationController
   def top
     # いいねの数が多い人気のページ順に表示
-    likes = Like.includes(:favorites).sort {|a,b| b.favorites.count <=> a.favorites.count}
-    @likes = Kaminari.paginate_array(likes).page(params[:page]).per(5)
+    @likes = Like.left_joins(:favorites).group(:id).order("count(favorites.like_id) desc").page(params[:page]).per(5)
+    # likes = Like.includes(:favorites).sort {|a,b| b.favorites.count <=> a.favorites.count}
+    # @likes = Kaminari.paginate_array(likes).page(params[:page]).per(5)
   end
 
   def about
